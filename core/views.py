7c8337d772093aff9737a99c998511afa8d77c5d
Eric <eacajolet@quinnipiac.edu>
2015-10-27 00:59:06 +0000

    Add success page

--- core/views.py
+++ core/views.py
@@ -7,9 +7,12 @@ from .models import *
 # Create your views here.
 class Home(TemplateView):
     template_name = "home.html"
-    
+
 class MessageCreateView(CreateView):
     model = Message
     template_name = "message/message_form.html"
     fields = ['title', 'description']
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('success')
+
+class Success(TemplateView):
+  template_name = "success.html"

3363dad3022e7e95757551e6bbf9456a30b00650
Eric <eacajolet@quinnipiac.edu>
2015-10-27 01:49:41 +0000

    Add messages to admin panel

--- core/models.py
+++ core/models.py
@@ -7,4 +7,4 @@ class Message(models.Model):
   message = models.TextField(null=True, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   def __unicode__(self):
-    return self.title
+    return self.name
